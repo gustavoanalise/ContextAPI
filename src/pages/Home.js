aa81c93ba2c938ad09fab9aa1040886e6c5fc461
Gustavo Analise Ferreira <63479834+gustavoanalise@users.noreply.github.com>
2022-05-15 17:32:09 -0300

    Alterando o contexto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import { useContext } from 'react';
 import { CounterContext } from '../context/CounterContext';
+import ChangeCounter from '../components/ChangeCounter';
 
 const Home = () => {
   const { counter } = useContext(CounterContext)
@@ -11,6 +12,8 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <p>Valor do contador: {counter}</p>
+      {/* 3 - alterando valor */}
+      <ChangeCounter />
     </div>
   )
 }
