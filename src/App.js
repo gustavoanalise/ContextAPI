e88f7d950499ae58b8dd308e1db06044323c0106
Gustavo Analise Ferreira <63479834+gustavoanalise@users.noreply.github.com>
2022-05-15 16:56:54 -0300

    Criando o provider e consertando alguns erros iniciais

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
-          <Route path='/contact' element={<Contact />}/>
+          <Route path='/contact' element={<Contact />} />
 
         </Routes>
 
